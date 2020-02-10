acb00c8a5c28a5c02702d314183b6c81fef60f41
Jatin <b4jatin@gmail.com>
2020-02-10 16:48:04 +1100

    game logic added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-console.log("Up and running..!!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
-console.log( "User Flipped" + " " + cardOne);
-console.log("User Flipped" + " " + cardThree);
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+//console.log("user flipped" + " " +cardsInPlay);
+let cardTwo = cards[0];
+cardsInPlay.push(cardTwo);
+console.log("user flipped" + " " +cardsInPlay[1]);
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match");
+	}
+	else {
+		alert("Sorry, try again");
+	}
+}
+
